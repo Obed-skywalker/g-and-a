e1545b6ddb714a10c4e73a643d44c0ddc8fc9cb7
obed renteria <ing7obed@gmail.com>
2024-04-15 14:22:34 -0600

    nabvar-click

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,15 @@
+let menu= document.querySelector('#bars-icon');
+let navbar= document.querySelector('.navbar');
+
+menu.onclick= () =>{
+    menu.classList.toggle('fa-times');
+    navbar.classList.toggle('active');
+}
+window.onscroll = ()=>{
+    menu.classList.remove('fa-times');
+    navbar.classList.remove('active');
+  }
+
 document.querySelectorAll('.box-container img').forEach(box =>{
     box.onclick = () =>{ 
     document.querySelector('.popup-image').style.display = 'block';
