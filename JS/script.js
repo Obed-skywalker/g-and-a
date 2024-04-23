697d20276041c449b77322c084d4cd66a21e8922
obed renteria <ing7obed@gmail.com>
2024-04-23 15:21:33 -0600

    fix popimage services

--- JS/script.js
+++ JS/script.js
@@ -10,10 +10,10 @@ window.onscroll = ()=>{
     navbar.classList.remove('active');
   }
 
-document.querySelectorAll('.box-container img').forEach(box =>{
-    box.onclick = () =>{ 
+document.querySelectorAll('.slide-container img').forEach(slidecontainer =>{
+    slidecontainer.onclick = () =>{ 
     document.querySelector('.popup-image').style.display = 'block';
-    document.querySelector('.popup-image img').src = box.getAttribute('src');
+    document.querySelector('.popup-image img').src = slidecontainer.getAttribute('src');
     }
 });
 document.querySelector('.popup-image span').onclick = () =>{
